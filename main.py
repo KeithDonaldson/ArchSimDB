a290f1452e8faaad134a7cdafbc4f66fa8f03c37
KeithDonaldson <krd@live.co.uk>
2016-10-05 18:50:20 +0100

    Now serving SVG instead of PNG, improved response time.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import time
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, send_file
 import os
 from werkzeug.utils import secure_filename
 
@@ -16,6 +16,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 TARGET = os.path.join(APP_ROOT, 'static/')
 ALLOWED_EXTENSIONS = set(['txt'])
 app.config['UPLOAD_FOLDER'] = TARGET
+chart = None
 
 
 @app.route("/")
@@ -33,18 +34,20 @@ def upload():
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
 
     flash('Program started')
     flash("=============")
     average_connectivity(file)
     connectivity_chart(file)
     flash("Program Complete")
+    file.close()
     return render_template('upload.html')
 
 
 def average_connectivity(file):
-    raw_data = pd.read_csv(file.filename, sep='\t', comment='#', names=["Source", "Destination"])
+    raw_data = pd.read_csv(TARGET + file.filename, sep='\t', comment='#', names=["Source", "Destination"])
     grouped_counted_data = raw_data.groupby(by="Source").count()
     mean_connectivity = grouped_counted_data['Destination'].mean()
 
@@ -54,14 +57,25 @@ def average_connectivity(file):
 
 
 def connectivity_chart(file):
-    raw_data = pd.read_csv(file.filename, sep='\t', comment='#', names=["Source", "Destination"])
+    raw_data = pd.read_csv(TARGET + file.filename, sep='\t', comment='#', names=["Source", "Destination"])
     counted_sorted_data = raw_data.groupby(by="Source").count().sort_values(by="Destination", ascending=False).rename(columns={'Destination' : 'Count'})
-    counted_sorted_data.plot.hist(bins=40)
-    plt.savefig(TARGET + "chart.png")
+    counted_sorted_data.plot.hist(bins=20).set_xlabel(file.filename + " Analysis")
+
+    plt.savefig(TARGET + "chart.svg")
+    #chart_path = TARGET + "chart.png?t=" + str(time.time())[:9]
+    #os.rename(TARGET + "chart.png", chart_path)
 
     flash("Sources with most connections:")
     flash(str(counted_sorted_data.iloc[0]))
     flash("=============")
+
+    #global chart
+    #chart = chart_path
+
+
+#@app.route("/get_image", methods=['GET', 'POST'])
+#def get_image():
+#    return send_file(chart, mimetype='image/png')
 
 
 if __name__ == '__main__':
