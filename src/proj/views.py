369be4560c8595180aaf49e6c5cec3254fef63df
Keith Donaldson <keith@localhost.localdomain>
2016-10-25 20:06:22 +0100

    Now using MongoDB and Pyramid.

---
+++ src/proj/views.py
@@ -0,0 +1,34 @@
+from pyramid.view import view_config
+
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def my_view(request):
+    info = databaseInfo()
+    collections = info.get_all_collection_names(request)
+    exp_cnt = info.get_experiment_count(request)
+    conf_cnt = info.get_configuration_count(request)
+    app_cnt = info.get_applications_count(request)
+
+    return {'collections': collections, 'exp_cnt': exp_cnt, 'conf_cnt': conf_cnt, 'app_cnt': app_cnt}
+
+
+class databaseInfo():
+
+    @staticmethod
+    def get_all_collection_names(request):
+        return request.db.collection_names()
+
+    @staticmethod
+    def get_collection_count(request, collection_name):
+        return request.db[collection_name].count()
+
+    @staticmethod
+    def get_experiment_count(request):
+        return request.db['experiments'].count()
+
+    @staticmethod
+    def get_configuration_count(request):
+        return request.db['configurations'].count()
+
+    @staticmethod
+    def get_applications_count(request):
+        return request.db['applications'].count()
