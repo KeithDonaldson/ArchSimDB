cde3dd60ce83b8899e4a77af228a3f5ddd02003f
Keith Donaldson <keith@localhost.localdomain>
2017-02-24 17:41:42 +0000

    Program now runs on local venv. Updated run.py

--- run.py
+++ run.py
@@ -55,28 +55,101 @@ def start_processes(args):
     """
 
     processes = []
+    envpath = args.envpath[0]
     atexit.register(end_processes, processes)
-    skip = None
+    skip_mongo = None
+    skip_mkdir = None
+
+    if args.setup:                               # Perform one-time setup
+
+        # - Make dbpath process
+        print("-" * 30)
+        print(">> Attempting to make directory at " + args.dbpath[0])
+        try:
+            os.mkdir(args.dbpath[0])
+            print(">> Made directory successfully")
+        except OSError as e:
+            if e.strerror == "File exists":
+                print(">> Directory already exists, moving on.")
+            else:
+                print(">> Couldn't create directory at " + args.dbpath[0])
+                print(e.strerror)
+                skip_mkdir = input(">> If this want to attempt to bypass this error (which may cause issues with "
+                                   "mongod), press [y] to skip. otherwise, press [n] to exit.\n")
+
+                while skip_mkdir not in ('y', 'n'):
+                    skip_mkdir = input(">> If this want to attempt to bypass this error (which may cause issues with "
+                                       "mongod), press [y] to skip. otherwise, press [n] to exit.\n")
+                if skip_mkdir == 'n':
+                    print(">> Exiting, failed to run.")
+                    exit()
+
+        # - Make VENV process
+        print("-" * 30)
+        print(">> Attempting to create virtual environment at " + envpath)
+        p_makevenv = subprocess.Popen(['python3', '-m', 'venv', envpath],
+                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        p_makevenv.communicate()
+
+        if p_makevenv.returncode == 0:
+            print(">> Venv created successfully at " + envpath)
+        else:
+            p_makevenv_stdout, p_makevenv_stderr = p_makevenv.communicate()[:2]
+            print_error('python -m venv ' + envpath, p_makevenv_stdout, p_makevenv_stderr)
+            print(">> Exiting, failed to run.")
+            exit()
+
+        # - Installing setuptools process
+        print("-" * 30)
+        print(">> Installing setuptools via pip3")
+        p_installsetuptools = subprocess.Popen([os.path.join(envpath, 'bin', 'pip3'), 'install', 'setuptools'],
+                                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        p_installsetuptools.communicate()
+
+        if p_installsetuptools.returncode == 0:
+            print(">> setuptools installed successfully")
+        else:
+            p_installsetuptools_stdout, p_installsetuptools_stderr = p_installsetuptools.communicate()[:2]
+            print_error('pip3 install setuptools ' + envpath, p_installsetuptools_stdout, p_installsetuptools_stderr)
+            print(">> Exiting, failed to run.")
+            exit()
+
+        # - Installing dependencies process
+        print("-" * 30)
+        print(">> Installing dependencies via pip3 (may take a few minutes)")
+        p_installdependencies = subprocess.Popen([os.path.join(envpath, 'bin', 'pip3'), 'install', '-e', '.'],
+                                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        p_installdependencies.communicate()
+
+        if p_installdependencies.returncode == 0:
+            print(">> Dependencies installed successfully")
+        else:
+            p_installdependencies_stdout, p_installdependencies_stderr = p_installdependencies.communicate()[:2]
+            print_error('pip3 install -e .' + envpath, p_installdependencies_stdout, p_installdependencies_stderr)
+            print(">> Exiting, failed to run.")
+            exit()
 
     # - Create mongod process
     print("-" * 30)
     print(">> Attempting to start mongod on dbpath " + args.dbpath[0])
     p_mongod = subprocess.Popen(['mongod', '--dbpath', args.dbpath[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    time.sleep(2)
+    time.sleep(3)
 
     if p_mongod.returncode is None:              # Program hasn't returned
         pass
     else:                                        # Program has failed to run
         p_mongod_stdout, p_mongod_stderr = p_mongod.communicate()[:2]
         print_error('mongod', p_mongod_stdout, p_mongod_stderr)
-        skip = input(">> If this error was because mongo if already running, press [y] to skip. Otherwise, "
-                     "press [n] to exit.\n")
+        skip_mongo = input(">> If this error was because mongo if already running, press [y] to skip. Otherwise, "
+                           "press [n] to exit.\n")
 
-        while skip not in ('y', 'n'):
-            skip = input(">> If this error was because mongo if already running, press [y] to skip. Otherwise, "
-                         "press [n] to exit.\n")
-        if skip == 'n':
+        while skip_mongo not in ('y', 'n'):
+            skip_mongo = input(">> If this error was because mongo if already running, press [y] to skip. Otherwise, "
+                               "press [n] to exit.\n")
+        if skip_mongo == 'n':
             print(">> Exiting, failed to run.")
             exit()
 
@@ -86,8 +159,8 @@ def start_processes(args):
     # - Create pserve process
     print("-" * 30)
     print(">> Attempting to start pserve")
-    p_pserve = subprocess.Popen(['pserve', '--reload', 'development.ini'])
-    time.sleep(2)
+    p_pserve = subprocess.Popen([os.path.join(envpath, 'bin', 'pserve'), '--reload', 'development.ini'])
+    time.sleep(1)
 
     if p_pserve.returncode is None:              # Program hasn't returned
         pass
@@ -103,7 +176,7 @@ def start_processes(args):
     # - At this point, all processes should be running
     print("-" * 30)
     print(">> Processes started successfully, ArchSimDB is now be available at http://localhost:" + str(args.port[0]))
-    if skip:
+    if skip_mongo:
         print(">> If the website doesn't load due to a timeout, it is likely because you skipped the mongod setup. The "
               "website will not load without the correctly setup mongod process running.")
 
@@ -177,6 +250,13 @@ if __name__ == '__main__':
                         help='The uri that your mongo daemon is to run on. (Default: %(default)s. Type: %(type)s)')
     parser.add_argument('--port', dest='port', type=int, nargs=1, default=[6543], required=False,
                         help='The port on localhost which ArchSimDB will run on. '
+                             '(Default: %(default)s. Type: %(type)s)')
+    parser.add_argument('--envpath', dest='envpath', type=str, nargs=1, default=['env/'],
+                        help='The absolute path to be used as the virtual environment. '
+                             '(Default: %(default)s. Type: %(type)s)')
+    parser.add_argument('--setup', dest='setup', type=bool, nargs=1, default=False,
+                        help='Determines whether to perform program setup such as creating the virtual environment, '
+                             'installing dependencies. Should only be performed once. '
                              '(Default: %(default)s. Type: %(type)s)')
 
     arguments = parser.parse_args()
